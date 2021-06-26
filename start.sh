7554a1b4bc9344e37b8991112b570bb4c6bf2ed6
Grey Johnson <grey.johnson@ufl.edu>
2021-06-25 19:07:27 -0700

    modified startup script to install cargo and build in release

--- start.sh
+++ start.sh
@@ -1,6 +1,8 @@
 sudo apt update
 sudo apt install nodejs
 sudo apt install npm
+curl https://sh.rustup.rs -sSf | sh -s -- -y
+source $HOME/.cargo/env
 
 git clone https://github.com/GreyJohnsonGit/grey_johnson_dev.git
 cd grey_johnson_dev
@@ -11,4 +13,4 @@ npm run build
 cd ../
 
 cd server
-cargo run
+cargo run --release
