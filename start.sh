54746f3ef47b749e0fe737b1370c6ffc4ee40ad5
Grey Johnson <grey.johnson@ufl.edu>
2021-06-25 20:04:00 -0700

    removed unnecesary sudos and added port redirect for http (not https)

--- start.sh
+++ start.sh
@@ -1,8 +1,11 @@
-sudo apt update
-sudo apt install nodejs
-sudo apt install npm
+# Run as Super User
+apt update
+apt install nodejs
+apt install npm
 curl https://sh.rustup.rs -sSf | sh -s -- -y
 source $HOME/.cargo/env
+
+/sbin/iptables -t nat -I PREROUTING --src 0/0 --dst 0/0 -p tcp --dport 80 -j REDIRECT --to-ports 3030
 
 git clone https://github.com/GreyJohnsonGit/grey_johnson_dev.git
 cd grey_johnson_dev
